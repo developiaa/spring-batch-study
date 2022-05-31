43cf450aa86764bd3c3bd7e410f6641e5796af1f
developiaa <developiaa@gmail.com>
2022-06-01 00:24:25 +0900

    first commit

---
+++ src/main/java/study/developia/batch/SpringBatchStudyApplication.java
@@ -0,0 +1,13 @@
+package study.developia.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/developia/batch/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.developia.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

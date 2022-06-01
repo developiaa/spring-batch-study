738da3989250938df7f3c7ed780e0497e7755ef2
developiaa <developiaa@gmail.com>
2022-06-01 14:31:26 +0900

    feat : Add batch job, step config

--- src/main/java/study/developia/batch/SpringBatchStudyApplication.java
+++ src/main/java/study/developia/batch/SpringBatchStudyApplication.java
@@ -1,8 +1,16 @@
 package study.developia.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 스프링 배치가 작동하기 위해 선언
+ * 총 4개의 설정 클래스 실행, 스프링 배치의 모든 초기화 및 실행 구성
+ * 빈으로 등록된 모든 job을 검색해서 초기화와 동시에 job을 수행하도록 구성
+ */
+
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchStudyApplication {
 

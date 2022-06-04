f5b571c99392ffb548b40efa68da1f5c4e903bc7
developiaa <developiaa@gmail.com>
2022-06-04 14:20:59 +0900

    feat : Add job parameter example

---
+++ src/main/java/study/developia/batch/jobparameter/JobParameterConfiguration.java
@@ -0,0 +1,70 @@
+package study.developia.batch.jobparameter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobParameterConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job2() {
+        return jobBuilderFactory.get("Job")
+                .start(step3())
+                .next(step4())
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+                        jobParameters.getString("name");
+                        jobParameters.getLong("seq");
+                        jobParameters.getDate("date");
+                        jobParameters.getDouble("age");
+
+                        Map<String, Object> jobParametersMap = chunkContext.getStepContext().getJobParameters();
+                        jobParametersMap.get("name");
+                        jobParametersMap.get("seq");
+                        jobParametersMap.get("date");
+                        jobParametersMap.get("age");
+
+                        System.out.println("step3 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 was executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
--- src/main/java/study/developia/batch/jobparameter/JobParameterTest.java
+++ src/main/java/study/developia/batch/jobparameter/JobParameterTest.java
@@ -1,4 +1,4 @@
-package study.developia.batch.jobinstance;
+package study.developia.batch.jobparameter;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
@@ -9,9 +9,11 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @RequiredArgsConstructor
 @Component
-public class JobRunner implements ApplicationRunner {
+public class JobParameterTest implements ApplicationRunner {
 
     private final JobLauncher jobLauncher;
     private final Job job;
@@ -19,7 +21,10 @@ public class JobRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
-                .addString("name", "user4")
+                .addString("name", "userName")
+                .addLong("seq", 2L)
+                .addDate("date", new Date())
+                .addDouble("age", 16.5)
                 .toJobParameters();
 
         jobLauncher.run(job, jobParameters);

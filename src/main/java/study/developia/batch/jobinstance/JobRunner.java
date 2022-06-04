3a258594a654d30bc055fcb5cec873faad1e0120
developiaa <developiaa@gmail.com>
2022-06-04 13:40:19 +0900

    feat : Add job instance example

---
+++ src/main/java/study/developia/batch/jobinstance/JobRunner.java
@@ -0,0 +1,27 @@
+package study.developia.batch.jobinstance;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("name", "user3")
+                .toJobParameters();
+
+        jobLauncher.run(job, jobParameters);
+    }
+}

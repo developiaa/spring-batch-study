f1d8c9f89ed7fdbc74f3f9d4cb7f1bd07f169bd7
developiaa <developiaa@gmail.com>
2023-04-07 22:14:44 +0900

    feat: Modify config

--- src/main/java/study/developia/batch/flowjob/FlowJobConfiguration.java
+++ src/main/java/study/developia/batch/flowjob/FlowJobConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class FlowJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;

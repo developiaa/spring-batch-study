0f073e16e0f4126164c55ce9be4e75e1bc59400a
developiaa <developiaa@gmail.com>
2023-06-23 00:00:21 +0900

    feat: Add JsonFileItemWriter example

--- src/main/java/study/developia/batch/stax/JsonFileItemWriterConfiguration.java
+++ src/main/java/study/developia/batch/stax/JsonFileItemWriterConfiguration.java
@@ -6,16 +6,15 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
-import org.springframework.batch.item.xml.StaxEventItemWriter;
-import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.oxm.Marshaller;
-import org.springframework.oxm.xstream.XStreamMarshaller;
 import study.developia.batch.jdbc.Customer;
 
 import javax.sql.DataSource;
@@ -23,32 +22,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RequiredArgsConstructor
-//@Configuration
-public class XmlStaxConfiguration {
+@Configuration
+public class JsonFileItemWriterConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final DataSource dataSource;
 
     @Bean
-    public Job xmlStaxEventItemWriterJob() {
-        return jobBuilderFactory.get("xmlStaxEventItemWriterJob")
+    public Job jsonFileItemWriterJob() {
+        return jobBuilderFactory.get("jsonFileItemWriterJob")
                 .incrementer(new RunIdIncrementer())
-                .start(xmlStaxEventItemWriterStep())
+                .start(jsonFileItemWriterStep())
                 .build();
     }
 
     @Bean
-    public Step xmlStaxEventItemWriterStep() {
-        return stepBuilderFactory.get("xmlStaxEventItemWriterStep")
+    public Step jsonFileItemWriterStep() {
+        return stepBuilderFactory.get("jsonFileItemWriterStep")
                 .<Customer, Customer>chunk(3)
-                .reader(xmlStaxEventItemReader())
-                .writer(xmlStaxEventItemWriter())
+                .reader(jsonFileItemReader())
+                .writer(jsonFileItemWriter())
                 .build()
                 ;
     }
 
     @Bean
-    public JdbcPagingItemReader<Customer> xmlStaxEventItemReader() {
+    public JdbcPagingItemReader<Customer> jsonFileItemReader() {
 
         JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
 
@@ -76,30 +75,13 @@ public class XmlStaxConfiguration {
     }
 
     @Bean
-    public StaxEventItemWriter xmlStaxEventItemWriter() {
-        return new StaxEventItemWriterBuilder<Customer>()
-                .name("customersWriter")
-                .marshaller(itemMarshaller())
-                .resource(new FileSystemResource("/Users/developia/dev/study/spring-batch-study/src/main/resources/customer2.xml"))
-                .rootTagName("customer")
-                .overwriteOutput(true)
-                .build();
-
-    }
-
-    @Bean
-    public Marshaller itemMarshaller() {
-        Map<String, Class<?>> aliases = new HashMap<>();
-        aliases.put("customer", Customer.class);
-        aliases.put("id", Long.class);
-        aliases.put("firstname", String.class);
-        aliases.put("lastname", String.class);
-        aliases.put("birthdate", String.class);
-
-        XStreamMarshaller xStreamMarshaller = new XStreamMarshaller();
-        xStreamMarshaller.setAliases(aliases);
-
-        return xStreamMarshaller;
+    public ItemWriter<? super Customer> jsonFileItemWriter() {
+        return new JsonFileItemWriterBuilder<Customer>()
+                .name("jsonFileItemWriter")
+                .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+                .resource(new FileSystemResource("/Users/developia/dev/study/spring-batch-study/src/main/resources/customer2.json"))
+                .build()
+                ;
     }
 
 }
